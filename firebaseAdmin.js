c245966cf78a59cae952ab716053da2847e79f27
sumit <sumitbagate3@gmail.com>
2025-09-06 17:16:49 +0530

    fix: refactor firebaseAdmin.js to support service account from environment variable

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,11 +1,19 @@
 const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
-    const serviceAccount = require("./config/serviceAccountKey.json");
+  let serviceAccount;
 
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-    });
+  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    // Parse from environment variable
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  } else {
+    // Fallback for local dev
+    serviceAccount = require("./config/serviceAccountKey.json");
+  }
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 }
 
 module.exports = admin;
